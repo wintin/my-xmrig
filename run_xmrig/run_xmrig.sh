109bfbbd95cc49700b8028ee71ccac57eb6c70a7
wintin <wintin@users.noreply.github.com>
2021-04-26 00:33:44 +0700

    Update run_xmrig.sh

--- run_xmrig/run_xmrig.sh
+++ run_xmrig/run_xmrig.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/wintin/my-xmrig.git && cd my-xmrig && chmod +x xmrig && rm -rf .git && sudo ./xmrig -c $1
+git clone https://github.com/wintin/my-xmrig.git && cd my-xmrig && chmod +x xmrig && rm -rf .git && rm -rf ./run_xmrig && sudo ./xmrig -c $1
